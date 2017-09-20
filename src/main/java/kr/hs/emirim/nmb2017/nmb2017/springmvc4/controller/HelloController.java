1c4a726c26577b9d7586f5346dfc082f2016c951
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 10:33:56 +0900

    타임리프 및 템플릿 엔진 사용

--- src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
@@ -2,14 +2,11 @@ package kr.hs.emirim.nmb2017.nmb2017.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	   @ResponseBody
 	   public String hello() {
-	      return "안녕하세요, 스프링!";
+	      return "resultPage";
 	   }
 
 }
