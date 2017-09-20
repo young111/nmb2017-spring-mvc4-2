c93fbc546f5e2b0330a9b041c12781e4ec92d00f
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 09:37:42 +0900

    스프링 프로젝트 시작

---
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/Nmb2017SpringMvc4Application.java
@@ -0,0 +1,12 @@
+package kr.hs.emirim.nmb2017.nmb2017.springmvc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nmb2017SpringMvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nmb2017SpringMvc4Application.class, args);
+	}
+}
---
+++ src/test/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/Nmb2017SpringMvc4ApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.nmb2017.nmb2017.springmvc4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Nmb2017SpringMvc4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
