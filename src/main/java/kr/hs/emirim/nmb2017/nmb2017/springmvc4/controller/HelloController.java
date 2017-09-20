63656a97271a68b882d699319feb84b2a6c2253a
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 11:01:45 +0900

    name 파라미터 처리 추가

--- src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
@@ -3,11 +3,12 @@ package kr.hs.emirim.nmb2017.nmb2017.springmvc4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	 public String hello(Model model) {
-		  model.addAttribute("message", "Hello from the controller"); 
+	 public String hello(@RequestParam("name") String userName, Model model) {
+		  model.addAttribute("message", userName + "님 안녕하세요!!");
 		  return "resultPage";
-		 }
+		}
 }
