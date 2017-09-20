6a93d06bba251d36860d074a2ad5943a860d539e
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 13:23:24 +0900

    트위터 검색 기능 추가

--- src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
@@ -1,14 +1,24 @@
 package kr.hs.emirim.nmb2017.nmb2017.springmvc4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HelloController {
-	@RequestMapping("/")
-	 public String hello(@RequestParam(defaultValue = "주인") String name, Model model) {
-		  model.addAttribute("message", name + "님 안녕하세요!!");
-		  return "resultPage";
-		}
+
+@Autowired private Twitter twitter;
+
+ @RequestMapping("/")
+ public String hello(@RequestParam(defaultValue = "고양이")
+ String search, Model model) {
+  SearchResults searchResults = twitter.searchOperations().search(search);
+  String text = searchResults.getTweets().get(0).getText(); 
+  model.addAttribute("message", text);
+  return "resultPage";
+ }
 }
