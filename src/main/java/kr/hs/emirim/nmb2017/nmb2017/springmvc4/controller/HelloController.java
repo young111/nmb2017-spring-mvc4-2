cc2b2c1ce50fde341e891b4b6f6e1be257c0c37f
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 11:04:06 +0900

    파라미터 기본값 넣어주기

--- src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	 public String hello(@RequestParam("name") String userName, Model model) {
-		  model.addAttribute("message", userName + "님 안녕하세요!!");
+	 public String hello(@RequestParam(defaultValue = "주인") String name, Model model) {
+		  model.addAttribute("message", name + "님 안녕하세요!!");
 		  return "resultPage";
 		}
 }
