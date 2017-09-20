07af50b0740a794d1ac305c001684aed3326bccd
youngsuk <cyeternal524@e-mirim.hs.kr>
2017-09-20 10:45:41 +0900

    message 값을 컨트롤레에서 넘기기

--- src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/nmb2017/springmvc4/controller/HelloController.java
@@ -1,12 +1,13 @@
 package kr.hs.emirim.nmb2017.nmb2017.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	   public String hello() {
-	      return "resultPage";
-	   }
-
+	 public String hello(Model model) {
+		  model.addAttribute("message", "Hello from the controller"); 
+		  return "resultPage";
+		 }
 }
